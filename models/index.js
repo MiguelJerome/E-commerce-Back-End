3fb92ebcb7a7bd9265234ac3da85f1349551e157
MiguelJerome <sharkmaycry@gmail.com>
2022-08-26 23:36:48 -0400

    final change

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag1',
+  as: 'tags',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag1',
+  as: 'products',
   foreignKey: 'tag_id'
 });
 
